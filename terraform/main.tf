9348e88b492f7524d91a1babbb0da856b69139b7
Reflection <artembrigaz@gmail.com>
2024-03-14 01:59:58 +0200

    sg,ec2 module

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,12 @@ module "vpc" {
   vpc_cidr    = var.vpc_cidr
   subnet_cidr = var.subnet_cidr
 }
+module "sg" {
+  source = "./module/sg"
+  vpc    = module.vpc.vpc
+}
+module "ec2" {
+  source = "./module/ec2"
+  sg     = module.sg.sg
+  subnet = module.vpc.subnet
+}
--- terraform/module/ec2/main.tf
+++ terraform/module/ec2/main.tf
@@ -1,1 +1,13 @@
+resource "aws_instance" "web" {
+  count                       = 1
+  ami                         = data.aws_ami.amazon-2.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = [var.sg]
+  subnet_id                   = var.subnet
+  availability_zone           = data.aws_availability_zones.available_zone.names[0]
 
+  tags = {
+    Name = "Web-server"
+  }
+}
